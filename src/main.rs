64eae23c5aee58750813f2519afb71648098d52b
Evan Kelch <evan.c.kelch@gmail.com>
2023-04-14 23:32:36 -0400

    implemented display trait for a struct!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Formatter;
 use axum::{Router};
 use axum::routing::get;
 
@@ -27,6 +28,16 @@ pub struct Work {
     pub done: bool,
 }
 
+impl std::fmt::Display for Work {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "id: {}, work code: {}, active: {}",
+            self.id, self.work_code, !self.done
+        )
+    }
+}
+
 async fn hello_world() -> String {
     String::from("Hello, world!")
 }
@@ -38,7 +49,13 @@ async fn give_work() {
         add_up_to: 100,
         done: false,
     };
+    println!("before all: {}", new_work);
 
     let work_json = serde_json::to_string(&new_work).unwrap();
-    println!("{work_json}");
+    println!("serialized: {work_json}");
+
+    let deserial_work: Work = serde_json::from_str(&work_json.as_str()).unwrap();
+    println!("deserialized: {}", deserial_work);
+
+    assert_eq!(new_work, deserial_work);
 }
